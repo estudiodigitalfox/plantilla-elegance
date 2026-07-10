c7e54497345627417d354721ff3cef4f6cc3fcff
estudiodigitalfox <estudiodigitalfox@gmail.com>
2026-07-10 11:59:40 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,33 @@
 const CONFIG = {
+
+  // ==========================
+  // DATOS DEL EVENTO
+  // ==========================
+
   nombreEvento: "XV Kimberly",
 
   fechaLimiteTexto: "19 de diciembre de 2026",
+
   fechaLimiteSistema: "2026-12-19T23:59:59",
 
-  colorPrincipal: "#8f5bd7",
-  colorSecundario: "#f2eaff",
+  // ==========================
+  // DISEÑO
+  // ==========================
+
+  colorPrincipal: "#8F5BD7",
+
+  colorSecundario: "#F2EAFF",
 
   animacion: "mariposas",
 
+  animacionColor: true,
+
+  // ==========================
+  // SISTEMA
+  // ==========================
+
+  modoConfirmacion: "elegance",
+
   scriptURL: ""
+
 };
