46d527cd15dea5208183d6171247241c7544735c
estudiodigitalfox <estudiodigitalfox@gmail.com>
2026-07-10 11:48:20 -0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+const CONFIG = {
+  nombreEvento: "XV Kimberly",
+
+  fechaLimiteTexto: "19 de diciembre de 2026",
+  fechaLimiteSistema: "2026-12-19T23:59:59",
+
+  colorPrincipal: "#8f5bd7",
+  colorSecundario: "#f2eaff",
+
+  animacion: "mariposas",
+
+  scriptURL: ""
+};
